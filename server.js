89e30540e89f2bb7011fde0ab3fb82eadf383ed2
Chris Mazzochi <cmazzochi81@gmail.com>
2018-04-27 13:38:00 -0400

    improved

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');//Variable for body-parser npm install
 const path = require('path');//to work with file and folder paths. 
 const http = require('http');//need to create an http server. 
 const app = express();//Bring in Express itself. 
-const api = require('./server/routes/api');
+//const api = require('./server/routes/api');
 
 // Setup parsers for hanlding incoming request data.
 app.use(bodyParser.json());
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 //Send all non api routes to the 'dist/index.html' file. Where the Angular app itself resides.
-app.use('/api', api);
+//app.use('/api', api);
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
