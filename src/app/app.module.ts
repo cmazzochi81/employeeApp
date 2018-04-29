f4a56532ffb82f12145365894a6423d3d2800d2e
Chris Mazzochi <cmazzochi81@gmail.com>
2018-04-29 13:11:38 -0400

    improved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatTableModule, MatSortModule} from '@angular/material';
 import {AppComponent} from './app.component';
 import {EmployeeTableComponent} from './employee-table/employee-table.component';
@@ -19,7 +21,9 @@ import {EmployeeService} from './employee.service';
     HttpClientModule,
     BrowserAnimationsModule,
     MatTableModule, 
-    MatSortModule
+    MatSortModule,
+    MatButtonModule,
+    MatButtonToggleModule
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
